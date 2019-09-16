24ff75cd6d07b69f7e5a53054099df8b38ad54b9
joaoMarinho94 <joaopaulim94@gmail.com>
2019-09-15 23:40:57 -0300

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { StatusBar } from 'react-native'
 
 import '~/config/ReactotronConfig';
 
 import Routes from '~/routes';
 
-const App = () => <Routes />;
+const App = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#8b16ae"/>
+    <Routes/>
+  </>
+);
 
 export default App;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,59 +1,43 @@
 import React from 'react';
 
-import {
-  Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar,
-} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    paddingHorizontal: 20,
-  },
-  fileName: {
-    fontWeight: 'bold',
-    marginTop: 5,
-  },
-  instructions: {
-    color: '#DDD',
-    fontSize: 14,
-    marginTop: 20,
-    textAlign: 'center',
-  },
-  logo: {
-    height: Dimensions.get('window').height * 0.11,
-    marginVertical: Dimensions.get('window').height * 0.11,
-    width: Dimensions.get('window').height * 0.11 * (1950 / 662),
-  },
-  welcome: {
-    color: '#fff',
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+import Header from '~/components/header'
+import Tabs from '~/components/tabs'
+import Menu from '~/components/menu'
 
-const Main = () => (
-  <ImageBackground
-    source={{
-      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
-    }}
-    style={styles.container}
-    resizeMode="cover"
-  >
-    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-    <Image
-      source={{
-        uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
-      }}
-      style={styles.logo}
-      resizeMode="contain"
-    />
-    <Text style={styles.welcome}>Bem-vindo ao Template Básico!</Text>
-    <Text style={styles.instructions}>Essa é a tela principal da sua aplicação =)</Text>
-    <Text style={styles.instructions}>Você pode editar a tela no arquivo:</Text>
-    <Text style={[styles.instructions, styles.fileName]}>src/pages/Main/index.js</Text>
-  </ImageBackground>
-);
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
-export default Main;
+export default function Main() {
+  return (
+    <Container>
+      <Header/>
+
+      <Content>
+
+        <Menu/>
+
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666"/>
+            <Icon name="visibility" size={28} color="#666"/>
+          </CardHeader>
+
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,65</Description>
+          </CardContent>
+
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Barbára Carvalho hoje às 10:00h
+            </Annotation>
+          </CardFooter>
+
+        </Card>
+      </Content>
+
+      <Tabs/>
+    </Container>
+  );
+}
