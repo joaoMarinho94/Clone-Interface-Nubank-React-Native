042e34de480bdbefdb5f6419943e34803eea2a09
joaoMarinho94 <joaopaulim94@gmail.com>
2019-09-19 23:57:57 -0300

    Last commit

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
+import { Animated } from 'react-native'
+import { PanGestureHandler, State } from 'react-native-gesture-handler'
+
 import Header from '~/components/header'
 import Tabs from '~/components/tabs'
 import Menu from '~/components/menu'
@@ -9,35 +12,86 @@ import Menu from '~/components/menu'
 import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
 export default function Main() {
+  let offset = 0;
+  const translateY = new Animated.Value(0);
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        }
+      }
+    ],
+    { useNativeDrive: true },
+  )
+
+  function onHandlerStateChange(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+      offset += translationY;
+      if (translationY >= 100){
+        opened = true;
+      }else{
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start( () => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
+
   return (
     <Container>
-      <Header/>
+      <Header />
 
       <Content>
+        <Menu translateY={translateY} />
 
-        <Menu/>
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChange}
+        >
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [-350, 0, 380],
+                outputRange: [-35, 0, 380],
+                extrapolate: 'clamp',
+              })
+            }],
+          }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility" size={28} color="#666" />
+            </CardHeader>
 
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666"/>
-            <Icon name="visibility" size={28} color="#666"/>
-          </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 197.611,65</Description>
+            </CardContent>
 
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$ 197.611,65</Description>
-          </CardContent>
-
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 20,00 recebida de Barbára Carvalho hoje às 10:00h
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebida de Barbára Carvalho hoje às 10:00h
             </Annotation>
-          </CardFooter>
+            </CardFooter>
 
-        </Card>
+          </Card>
+        </PanGestureHandler>
+
       </Content>
 
-      <Tabs/>
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
